0e17a165d9baa79f59fc120d8bb682c8c93a4bdc
Hellentorresr <htorres1@ucenfotec.ac.cr>
2022-09-08 15:02:40 -0600

    Creacion de la clase PlaylistVideos.java

---
+++ homeflixProyect/src/main/java/model/PlaylistVideos.java
@@ -0,0 +1,161 @@
+package model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ * Clase PlaylistVideos
+ */
+public class PlaylistVideos {
+    /**
+     * Atributos de la clase Playlist
+     */
+    private String namePlaylist;
+    private float totalPlayListDurationTime;
+    private String tema;
+    private LocalDate creationDate;
+    private int id;
+
+    private int idVideo;
+    private ArrayList<Video>videos = new ArrayList<>();
+
+    /**
+     * Metodo constructor
+     */
+
+    public PlaylistVideos(String namePlaylist, float totalPlayListDurationTime, String tema, LocalDate creationDate, int id, int idVideo, ArrayList<Video> videos) {
+        this.namePlaylist = namePlaylist;
+        this.totalPlayListDurationTime = totalPlayListDurationTime;
+        this.tema = tema;
+        this.creationDate = creationDate;
+        this.id = id;
+        this.idVideo = idVideo;
+        this.videos = videos;
+    }
+
+    /**
+     * Segundo metodo constructor
+     */
+    public PlaylistVideos() {
+    }
+
+    /**
+     * Tercer constructor
+     */
+    public PlaylistVideos(int id,String namePlaylist, float totalPlayListDurationTime, String tema, LocalDate creationDate,  int idVideo) {
+        this.namePlaylist = namePlaylist;
+        this.totalPlayListDurationTime = totalPlayListDurationTime;
+        this.tema = tema;
+        this.creationDate = creationDate;
+        this.id = id;
+        this.idVideo = idVideo;
+    }
+
+    public PlaylistVideos(String namePlaylist, float totalPlayListDurationTime, String tema, LocalDate creationDate, int id) {
+        this.namePlaylist = namePlaylist;
+        this.totalPlayListDurationTime = totalPlayListDurationTime;
+        this.tema = tema;
+        this.creationDate = creationDate;
+        this.id = id;
+    }
+
+    public PlaylistVideos(int idVideo, int idPlaylist) {
+        this.id = idPlaylist;
+        this.idVideo = idVideo;
+    }
+
+
+    /**
+     * Getters y setters
+     */
+
+    public String getNamePlaylist() {
+        return namePlaylist;
+    }
+
+    public void setNamePlaylist(String namePlaylist) {
+        this.namePlaylist = namePlaylist;
+    }
+
+    public float getTotalPlayListDurationTime() {
+        return totalPlayListDurationTime;
+    }
+
+    public void setTotalPlayListDurationTime(float totalPlayListDurationTime) {
+        this.totalPlayListDurationTime = totalPlayListDurationTime;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdVideo() {
+        return idVideo;
+    }
+
+    public void setIdVideo(int idVideo) {
+        this.idVideo = idVideo;
+    }
+
+    public ArrayList<Video> getVideos() {
+        return videos;
+    }
+
+    public void setVideos(ArrayList<Video> videos) {
+        this.videos = videos;
+    }
+
+    public void agregarVideo(Video video){
+        this.videos.add(video);
+    }
+    public void setTotalPlayListDurationTime(Video video) {
+        this.totalPlayListDurationTime+=video.getTotalDuration();
+    }
+
+    /**
+     * Metodo toString
+     */
+    @Override
+    public String toString() {
+        return "PlaylistVideos{" +
+                "namePlaylist='" + namePlaylist + '\'' +
+                ", totalPlayListDurationTime=" + totalPlayListDurationTime +
+                ", tema='" + tema + '\'' +
+                ", creationDate=" + creationDate +
+                ", id=" + id +
+                ", idVideo=" + idVideo +
+                ", videos=" + videos +
+                '}';
+    }
+
+    /**
+     * Metodo equals
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PlaylistVideos that)) return false;
+        return Float.compare(that.getTotalPlayListDurationTime(), getTotalPlayListDurationTime()) == 0 && getId() == that.getId() && getIdVideo() == that.getIdVideo() && Objects.equals(getNamePlaylist(), that.getNamePlaylist()) && Objects.equals(getTema(), that.getTema()) && Objects.equals(getCreationDate(), that.getCreationDate()) && Objects.equals(getVideos(), that.getVideos());
+    }
+}
